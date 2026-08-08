ffcb87f5223d571015ba08c8dd5489f4fd6c2327
Pep190126 <giuseppedapozzo@outlook.com>
2026-08-08 14:22:50 +0200

    Run edition metadata self-test

--- src/Diez.Desktop/Program.cs
+++ src/Diez.Desktop/Program.cs
@@ -19,6 +19,7 @@ internal static class Program
             {
                 PackageSelfTest.RunAsync().GetAwaiter().GetResult();
                 EditableMasterSelfTest.RunAsync().GetAwaiter().GetResult();
+                EditionMetadataSelfTest.RunAsync().GetAwaiter().GetResult();
                 EditionFreezeSelfTest.RunAsync().GetAwaiter().GetResult();
                 PublicationCandidateSelfTest.RunAsync().GetAwaiter().GetResult();
                 return 0;
