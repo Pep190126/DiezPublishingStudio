880d8d857af9d38e57a2c9e5310f8af6721cf354
Pep190126 <giuseppedapozzo@outlook.com>
2026-08-09 10:41:39 +0200

    Run Word Search workspace self-test

--- src/Diez.Desktop/Program.cs
+++ src/Diez.Desktop/Program.cs
@@ -31,6 +31,7 @@ internal static class Program
                 ProductionPackageSelfTest.RunAsync().GetAwaiter().GetResult();
                 AiProductionSelfTest.RunAsync().GetAwaiter().GetResult();
                 AiImageBatchSelfTest.RunAsync().GetAwaiter().GetResult();
+                WordSearchWorkspaceSelfTest.RunAsync().GetAwaiter().GetResult();
                 return 0;
             }
             catch (Exception ex)
