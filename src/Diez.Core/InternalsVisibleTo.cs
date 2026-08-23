207f3ab5947239bd5dc3fcaf639b0909ca8a97ed
Pep190126 <giuseppedapozzo@outlook.com>
2026-08-23 19:57:52 +0200

    Allow visual pianist to test internal semantic contracts

--- src/Diez.Core/InternalsVisibleTo.cs
+++ src/Diez.Core/InternalsVisibleTo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("DiezPublishingStudio")]
 [assembly: InternalsVisibleTo("Diez.Uno")]
 [assembly: InternalsVisibleTo("Diez.Core.Pianist")]
+[assembly: InternalsVisibleTo("Diez.VisualBook.Pianist")]
