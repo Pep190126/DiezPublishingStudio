3c59bece07dce956a8837132e2f342cf172fbb42
Pep190126 <giuseppedapozzo@outlook.com>
2026-08-24 11:47:17 +0200

    Retrigger Round 5.5 compile fix

--- src/Diez.Uno/Round55ApplyMarker.cs
+++ src/Diez.Uno/Round55ApplyMarker.cs
@@ -3,5 +3,5 @@ namespace DiezPublishingStudio.UnoSpike;
 /// <summary>One-shot trigger for Round 5.5 editable pre-accept subject proposal compile fix.</summary>
 internal static class Round55ApplyMarker
 {
-    public const string Candidate = "Round 5.5 editable subject proposal compile fix";
+    public const string Candidate = "Round 5.5 editable subject proposal compile fix 2";
 }
